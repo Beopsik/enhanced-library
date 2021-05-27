5630cd93db0ee85aeea5959eda44b5441f74e147
Moon Beob Sik <tjswl756@naver.com>
2021-05-28 02:36:04 +0900

    feat:modify dto

--- src/main/java/enhancedLibrary/web/dto/IssuesResponseDto.java
+++ src/main/java/enhancedLibrary/web/dto/IssuesResponseDto.java
@@ -3,6 +3,10 @@ package enhancedLibrary.web.dto;
 import enhancedLibrary.domain.Issues;
 import lombok.Getter;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Getter
 public class IssuesResponseDto {
     private String guestId;
@@ -14,8 +18,12 @@ public class IssuesResponseDto {
     private String dueDate;
     private boolean overdueState;
     private int calculatedFine;
-
-    public IssuesResponseDto(Issues entity){
+    /*Date today=new Date();
+    String startDate = new SimpleDateFormat("YYYY-MM-dd").format(today);
+    Date due = new Date(today.getTime() + 14*(1000 * 60 * 60 * 24));
+    String dueDate=new SimpleDateFormat("YYYY-MM-dd").format(due);*/
+    public IssuesResponseDto(Issues entity) {
+        Date today=new Date();
         this.guestId=entity.getGuestId();
         this.bookId=entity.getBookId().getBookId();
         this.title=entity.getBookId().getTitle();
@@ -23,7 +31,17 @@ public class IssuesResponseDto {
         this.bookType=entity.getBookId().getBookType();
         this.startDate=entity.getStartDate();
         this.dueDate=entity.getDueDate();
-        this.overdueState=entity.getOverdueState();
-        this.calculatedFine=entity.getCalculatedFine();
+        try {
+            Date due=new SimpleDateFormat("yyyy-MM-dd").parse(dueDate);
+            this.overdueState = today.after(due);
+            if(this.overdueState) {
+                Long num=(today.getTime()-due.getTime())/(1000 * 60 * 60 * 24);
+                this.calculatedFine =num.intValue()*1000;
+            }else{
+                this.calculatedFine=entity.getCalculatedFine();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
